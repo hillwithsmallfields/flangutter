3f2cbc54acd0b08c9a9135cad56365af94ed07fe
John Sturdy <jcg.sturdy@gmail.com>
2015-08-13 20:06:07 +0100

    Experimenting to get it loaded

--- src/flangutter/core.clj
+++ src/flangutter/core.clj
@@ -1,5 +1,9 @@
 (ns flangutter.core
-  (:gen-class))
+  ;; I'll move some of the code into a library once I'm more comfortable with how the infrastructure works
+  ;; (:require [natlangfun :as lang])
+  (:gen-class)
+  (:require csv-map.core
+            [clojure.java.io :as io]))
 
 (defn -main
   "I don't do a whole lot ... yet."
