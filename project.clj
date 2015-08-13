c79d55066353024f44259a21350031f298d23b19
John Sturdy <jcg.sturdy@gmail.com>
2015-08-13 20:05:57 +0100

    Experimenting to get it loaded

--- project.clj
+++ project.clj
@@ -3,7 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  ;; :repositories [
+  ;;                ;; point to wherever natlangfun goes
+  ;;                ]
+  ;; :import (java.util.jar JarFile)
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [csv-map "0.1.2"]
+                 [clojure.java.io :as io]
+                 ;; todo: put natlangfun here when I separate it out
+                 ]
   :main ^:skip-aot flangutter.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
